96ba936c59c97b399353923952cf90fee93881d1
Andrew <andrew@myfuturesuper.com.au>
2021-11-25 21:49:38 +1100

    feat: sitemap false

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,8 @@
-export default {
-  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  // pages: './src/pages', // Path to Astro components, pages, and data
-  // dist: './dist',       // When running `astro build`, path to final static output
-  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
+export default /** @type {import('astro').AstroUserConfig} */ ({
+  // Comment out "renderers: []" to enable Astro's default component support.
+  renderers: ["@astrojs/renderer-svelte"],
   buildOptions: {
-    // site: 'http://example.com',           // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-    sitemap: true,         // Generate sitemap (set to "false" to disable)
+    sitemap: false,
+    site: "https://www.impactwrap.futuresuper.com.au/",
   },
-  devOptions: {
-    // hostname: 'localhost',  // The hostname to run the dev server on.
-    // port: 3000,             // The port to run the dev server on.
-  },
-  renderers: [],
-};
+});
