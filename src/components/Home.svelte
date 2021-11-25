24afec2ad1c1ab69762d0b35b7bb252d15bc5fbb
Andrew <andrew@myfuturesuper.com.au>
2021-11-25 22:31:41 +1100

    feat: set up home page

---
+++ src/components/Home.svelte
@@ -0,0 +1,22 @@
+<script>
+  const clientSide = !import.meta.env.SSR;
+
+  if (clientSide) {
+    postData(
+      "https://67l8qspd50.execute-api.ap-southeast-2.amazonaws.com/prod/letter?rcode="
+    );
+  }
+
+  async function postData(url = "", data = {}) {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    console.log(response);
+  }
+</script>
+
+<div>Hello world</div>
