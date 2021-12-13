dab6362b9dcfc9134d5a5ba5df5247a257378a8d
Andrew <andrew@myfuturesuper.com.au>
2021-12-13 15:09:27 +1100

    feat: update cid

--- src/components/Home.svelte
+++ src/components/Home.svelte
@@ -3,29 +3,29 @@
 
   const clientSide = !import.meta.env.SSR;
 
-  // const cid = "90aaedd7-7329-4ac5-bfa9-6dd586300c2f";
+  const cid = "94f7111e-264d-4089-9db2-2c49b7f89eea";
 
-  // const url1 = "https://api.fsstaging.com.au/api/auth-user";
-  // const url2 = "https://api.fsstaging.com.au/api/members/045972";
+  const url1 = "https://api.fsstaging.com.au/api/auth-user";
+  const url2 = "https://api.fsstaging.com.au/api/members/045972";
 
-  // if (clientSide) {
-  //   getData(url1);
-  //   getData(url2);
-  // }
+  if (clientSide) {
+    getData(url1);
+    getData(url2);
+  }
 
-  // async function getData(url = "") {
-  //   const response = await fetch(url, {
-  //     method: "GET",
-  //     headers: {
-  //       Accept: "application/json",
-  //       "Content-Type": "application/json",
-  //       "Access-Control-Allow-Origin": "https://api.fsstaging.com.au",
-  //       cid: cid,
-  //     },
-  //     credentials: "include",
-  //   });
-  //   console.log(response);
-  // }
+  async function getData(url = "") {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "https://api.fsstaging.com.au",
+        cid: cid,
+      },
+      credentials: "include",
+    });
+    console.log(response);
+  }
 </script>
 
 <div class="container">
